384b737191729959b19ee632241463edf8c180fc
repelliuss <repelliuss@gmail.com>
2021-05-09 19:21:10 +0300

    Update notifier signal names

--- src/core/Notifier.cpp
+++ src/core/Notifier.cpp
@@ -5,12 +5,12 @@ using namespace godot;
 void Notifier::_register_methods() {
   register_method("_process", &Notifier::_process);
 
-  register_signal<Notifier>((char *)"ball_position_changed", "ball_position_x",
+  register_signal<Notifier>((char *)"on_ball_position_changed", "ball_position_x",
                             GODOT_VARIANT_TYPE_INT, "ball_position_z",
                             GODOT_VARIANT_TYPE_INT);
-  register_signal<Notifier>((char *)"arm_snt_position_changed",
+  register_signal<Notifier>((char *)"on_arm_snt_position_changed",
                             "arm_snt_position", GODOT_VARIANT_TYPE_INT);
-  register_signal<Notifier>((char *)"arm_gk_position_changed",
+  register_signal<Notifier>((char *)"on_arm_gk_position_changed",
                             "arm_gk_position", GODOT_VARIANT_TYPE_INT);
 }
 
@@ -20,9 +20,9 @@ void Notifier::_init() {}
 
 void Notifier::_process(float delta) {
   if (listener.next(data.raw) == 0) {
-    emit_signal("ball_position_changed", data.get_ball_position_x(),
+    emit_signal("on_ball_position_changed", data.get_ball_position_x(),
                 data.get_ball_position_z());
-    emit_signal("arm_snt_position_changed", data.get_arm_snt_position());
-    emit_signal("arm_gk_position_changed", data.get_arm_gk_position());
+    emit_signal("on_arm_snt_position_changed", data.get_arm_snt_position());
+    emit_signal("on_arm_gk_position_changed", data.get_arm_gk_position());
   }
 }
