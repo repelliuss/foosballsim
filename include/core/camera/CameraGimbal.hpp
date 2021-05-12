ae7e2977aa2bc983feed3fea15af30033ce7f94b
realsontaran <e.ozbay75@gmail.com>
2021-05-12 16:35:56 +0300

    CameraGimbal

---
+++ include/core/camera/CameraGimbal.hpp
@@ -0,0 +1,26 @@
+#ifndef CAMERAGIMBAL_HPP_
+#define CAMERAGIMBAL_HPP_
+
+#include <Godot.hpp>
+#include <Input.hpp>
+#include <Spatial.hpp>
+
+#define PI 3.141593
+
+namespace godot {
+class CameraGimbal : public Spatial {
+  GODOT_CLASS(CameraGimbal, Spatial)
+public:
+  static void _register_methods();
+  CameraGimbal() noexcept;
+  void _init();
+  void _process(float delta);
+  void get_input_keyboard(float delta);
+
+private:
+  float rotation_speed;
+  Input *input;
+};
+} // namespace godot
+
+#endif // CAMERAGIMBAL_HPP_
---
+++ src/core/camera/CameraGimbal.cpp
@@ -0,0 +1,49 @@
+#include <Defs.hpp>
+#include <Vector3.hpp>
+#include <camera/CameraGimbal.hpp>
+using namespace godot;
+
+void CameraGimbal::_register_methods() {
+  register_method("_process", &CameraGimbal::_process);
+  register_method("get_input_keyboard", &CameraGimbal::get_input_keyboard);
+  register_property<CameraGimbal, float>("rotation_speed",
+                                         &CameraGimbal::rotation_speed, PI / 2);
+}
+
+CameraGimbal::CameraGimbal() noexcept {}
+
+void CameraGimbal::_process(float delta) {
+  // get_input_keyboard(delta);
+}
+
+void CameraGimbal::_init() {
+  input = Input::get_singleton();
+  rotation_speed = PI / 2;
+}
+
+void CameraGimbal::get_input_keyboard(float delta) {
+  int y_rot = 0;
+  int x_rot = 0;
+  auto v3 = Vector3();
+
+  if (input->is_action_pressed("cam_right")) {
+    y_rot += 1;
+  }
+  if (input->is_action_pressed("cam_left")) {
+    y_rot -= 1;
+  }
+
+  rotate_object_local(v3.UP, y_rot * rotation_speed * delta);
+
+  if (input->is_action_pressed("cam_up")) {
+    x_rot += 1;
+  }
+  if (input->is_action_pressed("cam_down")) {
+    x_rot -= 1;
+  }
+
+  Node *node = get_node("InnerGimbal");
+  Spatial *innerGimbal = nullptr;
+  innerGimbal = Object::cast_to<Spatial>(node);
+  innerGimbal->rotate_object_local(v3.RIGHT, x_rot * rotation_speed * delta);
+}
