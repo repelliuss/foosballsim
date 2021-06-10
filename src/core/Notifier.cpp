dcd9a9f493fd1d756b7ca6a37e5c4ee6a6eca66a
repelliuss <repelliuss@gmail.com>
2021-06-11 01:16:08 +0300

    Fix sending out of bound positions

--- src/core/Notifier.cpp
+++ src/core/Notifier.cpp
@@ -34,22 +34,42 @@ void Notifier::_process(float delta) {
     int human_gk_position = data.get_human_gk_position();
 
     if (ball_position_x >= 0 && ball_position_x <= org_x_length &&
-        ball_position_z >= 0 && ball_position_z <= org_z_length &&
-        arm_snt_position >= 0 && arm_snt_position <= org_z_length &&
-        arm_gk_position >= 0 && arm_gk_position <= org_z_length) {
+        ball_position_z >= org_z_offset && ball_position_z <= org_z_length) {
       emit_signal("on_ball_position_changed", ball_position_x, ball_position_z);
+    }
+
+    if (arm_snt_position >= org_z_offset && arm_snt_position <= org_z_length) {
       emit_signal("on_arm_snt_position_changed", arm_snt_position);
+    }
+
+    if (arm_gk_position >= org_z_offset && arm_gk_position <= org_z_length) {
       emit_signal("on_arm_gk_position_changed", arm_gk_position);
-      emit_signal("on_human_snt_position_changed", human_snt_position);
+    }
+
+    if (human_gk_position >= org_z_offset &&
+        human_gk_position <= org_z_length) {
       emit_signal("on_human_gk_position_changed", human_gk_position);
+    }
+
+    if (human_snt_position >= org_z_offset &&
+        human_snt_position <= org_z_length) {
+      emit_signal("on_human_snt_position_changed", human_snt_position);
     }
   }
 }
+
 int Notifier::Data::get_ball_position_x() const noexcept { return raw[0]; }
+
 int Notifier::Data::get_ball_position_z() const noexcept { return raw[1]; }
+
 int Notifier::Data::get_arm_snt_position() const noexcept { return raw[2]; }
+
 int Notifier::Data::get_arm_gk_position() const noexcept { return raw[3]; }
+
 int Notifier::Data::get_human_gk_position() const noexcept { return raw[4]; }
+
 int Notifier::Data::get_human_snt_position() const noexcept { return raw[5]; }
+
 int Notifier::Data::get_score_human() const noexcept { return raw[6]; }
+
 int Notifier::Data::get_score_robot() const noexcept { return raw[7]; }
